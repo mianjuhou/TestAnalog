673f84ce7f27663da30b7139c45881f56352185e
fda <fangdean@yeah.net>
2019-05-31 10:50:58 +0800

    添加前端页面

---
+++ analog/src/test/java/com/potevio/analog/TestIp.java
@@ -0,0 +1,41 @@
+package com.potevio.analog;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedMap;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestIp {
+
+    @Test
+    public void testIp() {
+
+//        ImmutableMap immutableMap = ImmutableMap.<String, String>builder()
+//                .put("a", "1")
+//                .put("e", "5")
+//                .put("b", "2")
+//                .put("c", "3")
+//                .put("g", "7")
+//                .put("d", "4")
+//                .put("f", "6")
+//                .build();
+//        System.out.println(immutableMap);
+//        ImmutableSortedMap sortedMap = ImmutableSortedMap.copyOf(immutableMap);
+//        sortedMap.forEach((key, value) -> {
+//            System.out.println(key + ":" + value);
+//        });
+
+//        Set<String> stationId = new HashSet<>();
+//        stationId.add("a");
+//        stationId.add("b");
+//        stationId.add("a");
+//        stationId.add("c");
+//        stationId.add("d");
+//        stationId.add("e");
+//        System.out.println(stationId);
+
+    }
+
+}
